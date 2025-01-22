636fdbd07c6a082c7748b9609704caa69ed79fd1
Yigit ozcelep <yoroso85onurlu@outlook.com>
2025-01-22 16:21:49 +0300

    fnish kart part

--- app/src/main/java/com/example/memory/MainActivity.kt
+++ app/src/main/java/com/example/memory/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.memory
 
-import android.opengl.Visibility
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import android.widget.Button
@@ -11,26 +10,31 @@ import android.os.SystemClock
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.graphics.Color
-import org.w3c.dom.Text
+import android.os.Handler
+import android.os.Looper
+import android.widget.EditText
 
 
 class MainActivity : ComponentActivity() {
-    private var selectedCards           : MutableList<Int> = mutableListOf()
+    private var userAnswer              : MutableList<Int> = mutableListOf()
+    private var expectedAnswer          : MutableList<Int> = mutableListOf()
     private var startTime               : Long = 0L
     private var resultTime              : Long = 0L
-    private var wrongCount              : Long = 0L
+    private var correctCount            : Long = 0L
+    private val handler                 : Handler = Handler(Looper.getMainLooper())
+
+    private lateinit var kartNextButton : Button
+    private lateinit var sayiNextButton : Button
     private lateinit var kartButton     : Button
     private lateinit var sayiButton     : Button
-    private lateinit var fnishButton    : Button
     private lateinit var endTimeText    : TextView
-    private lateinit var totalWrongText : TextView
-
+    private lateinit var totalCorText   : TextView
+    private lateinit var corSizeText    : TextView
+    private lateinit var timeInput      : EditText
     private lateinit var gridLayout     : GridLayout
-    private lateinit var kartNextButton : Button
 
 
-    private val cardImgs = mutableListOf(
-        R.drawable.clubs_a, R.drawable.clubs_2, R.drawable.clubs_3,
+    private val defaultOrderCardsImgs = mutableListOf(R.drawable.clubs_a, R.drawable.clubs_2, R.drawable.clubs_3,
         R.drawable.clubs_4, R.drawable.clubs_5, R.drawable.clubs_6,
         R.drawable.clubs_7, R.drawable.clubs_8, R.drawable.clubs_9,
         R.drawable.clubs_10, R.drawable.clubs_j, R.drawable.clubs_q, R.drawable.clubs_k,
@@ -45,11 +49,7 @@ class MainActivity : ComponentActivity() {
         R.drawable.spades_a, R.drawable.spades_2, R.drawable.spades_3,
         R.drawable.spades_4, R.drawable.spades_5, R.drawable.spades_6,
         R.drawable.spades_7, R.drawable.spades_8, R.drawable.spades_9,
-        R.drawable.spades_10, R.drawable.spades_j, R.drawable.spades_q, R.drawable.spades_k
-    )
-
-    private val defaultOrderCardsImgs = cardImgs.toMutableList()
-
+        R.drawable.spades_10, R.drawable.spades_j, R.drawable.spades_q, R.drawable.spades_k)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -61,33 +61,67 @@ class MainActivity : ComponentActivity() {
     private fun addListenersToButtons() {
         kartButton.setOnClickListener  {showKartPage()}
         sayiButton.setOnClickListener  {showSayiPage()}
-        fnishButton.setOnClickListener {showStartPage()}
         kartNextButton.setOnClickListener {
             resultTime = SystemClock.elapsedRealtime() - startTime
             kartNextButton.visibility = View.GONE
             showOrderCardsPage()
         }
     }
+    fun generateRandomStrings(): List<String> {
+        return List(52) {
+            (1..3).map { ('0'..'9').random() }.joinToString("")
+        }
+    }
+
+    private fun showNumbers() {
+        val numbers = generateRandomStrings()
+        for (number in numbers) {
+            val textView = TextView(this).apply {
+                textSize = 22f
+                setPadding(16, 16, 16, 16)
+                setBackgroundResource(android.R.color.darker_gray)
+                layoutParams = GridLayout.LayoutParams().apply {
+                    width = GridLayout.LayoutParams.WRAP_CONTENT
+                    height = GridLayout.LayoutParams.WRAP_CONTENT
+                    setMargins(16, 45, 16, 16)
+                }
+            }
+            gridLayout.addView(textView)
+        }
+    }
 
     private fun showSayiPage() {
+        val minute = timeInput.text.toString().toIntOrNull() ?: return
         performCommonsOfPages()
+        sayiNextButton.visibility = View.VISIBLE
+
+        handler.postDelayed({
+
+
+        }, (minute * 60 * 1000).toLong())
+
+
+
     }
 
     private fun performCommonsOfPages() {
         hideInitialPage()
         startTime = SystemClock.elapsedRealtime()
+        gridLayout.visibility = View.VISIBLE
+        gridLayout.removeAllViews()
     }
 
     private fun hideInitialPage() {
-        kartButton.visibility  = View.GONE
-        sayiButton.visibility  = View.GONE
-        endTimeText.visibility = View.GONE
-        totalWrongText.visibility = View.GONE
+        kartButton.visibility   = View.GONE
+        sayiButton.visibility   = View.GONE
+        endTimeText.visibility  = View.GONE
+        totalCorText.visibility = View.GONE
+        timeInput.visibility    = View.GONE
+        corSizeText.visibility  = View.GONE
     }
 
     private fun showCards(cards: List<Int>, onCardClick: (cardResource: Int, imageView: ImageView) -> Unit = { _, _ -> }){
         gridLayout.removeAllViews()
-        gridLayout.visibility = View.VISIBLE
         for (cardResource in cards) {
             val imageView = ImageView(this)
             imageView.setImageResource(cardResource)
@@ -105,41 +139,43 @@ class MainActivity : ComponentActivity() {
 
     private fun showOrderCardsPage() {
         showCards(defaultOrderCardsImgs) { cardResource, imageView ->
-            if (selectedCards.contains(cardResource)) {
-                selectedCards.remove(cardResource)
+            if (userAnswer.contains(cardResource)) {
+                userAnswer.remove(cardResource)
                 imageView.setBackgroundColor(Color.TRANSPARENT)
             } else {
-                selectedCards.add(cardResource)
+                userAnswer.add(cardResource)
                 imageView.setBackgroundColor(Color.GREEN)
             }
-            if (selectedCards.size == 52){
-                wrongCount = getDiff(cardImgs, selectedCards)
+            if (userAnswer.size == 52){
+                correctCount = getCorrectCount(expectedAnswer, userAnswer)
                 showStartPage()
             }
         }
     }
 
-    private fun getDiff(currentList: MutableList<Int>, answerList: MutableList<Int>): Long {
+    private fun getCorrectCount(currentList: MutableList<Int>, answerList: MutableList<Int>): Long {
         val minSize = minOf(currentList.size, answerList.size)
         var count = 0L
         for (i in 0 until minSize) {
-            if (currentList[i] != answerList[i]) count++
+            if (currentList[i] == answerList[i]) count++
         }
         return count
     }
-    
+
     private fun showKartPage() {
         performCommonsOfPages()
-        cardImgs.shuffle()
+        expectedAnswer = defaultOrderCardsImgs.toMutableList()
+        expectedAnswer.shuffle()
         kartNextButton.visibility = View.VISIBLE
-        showCards(cardImgs)
+        showCards(expectedAnswer)
     }
 
     private fun hidePages() {
-        selectedCards.clear()
+        userAnswer.clear()
+        expectedAnswer.clear()
         gridLayout.visibility  = View.GONE
-        fnishButton.visibility = View.GONE
         gridLayout.visibility  = View.GONE
+        sayiNextButton.visibility = View.GONE
     }
 
     private fun showTime() {
@@ -150,35 +186,44 @@ class MainActivity : ComponentActivity() {
         startTime = 0L
     }
 
-    private fun showWrong() {
-        totalWrongText.visibility = View.VISIBLE
-        totalWrongText.text = "Total Wrong: $wrongCount"
+    private fun showCorrect() {
+        totalCorText.visibility = View.VISIBLE
+        totalCorText.text = "Total Correct: $correctCount"
     }
 
+    private fun showSize() {
+        corSizeText.visibility = View.VISIBLE
+        corSizeText.text       = "Total Size: ${expectedAnswer.size}"
+    }
     private fun showStartPage() {
+        showSize()
         hidePages()
         showTime()
-        showWrong()
+        showCorrect()
         kartButton.visibility = View.VISIBLE
         sayiButton.visibility = View.VISIBLE
-
-
+        timeInput.visibility  = View.VISIBLE
     }
 
     private fun initializeVariables() {
         kartButton     = findViewById<Button>(R.id.button1)
         sayiButton     = findViewById<Button>(R.id.button2)
-        fnishButton    = findViewById<Button>(R.id.finishButton)
-        gridLayout     = findViewById<GridLayout>(R.id.cardGridLayout)
-        endTimeText    = findViewById<TextView>(R.id.endTimeText)
+        sayiNextButton = findViewById<Button>(R.id.sayiNextButton)
         kartNextButton = findViewById<Button>(R.id.kartNextButton)
-        totalWrongText = findViewById<TextView>(R.id.totalWrongText)
+        endTimeText    = findViewById<TextView>(R.id.endTimeText)
+        totalCorText   = findViewById<TextView>(R.id.totalCorrectText)
+        corSizeText    = findViewById<TextView>(R.id.totalSizeText)
+        timeInput      = findViewById<EditText>(R.id.timeInput)
+        endTimeText    = findViewById<TextView>(R.id.endTimeText)
+        gridLayout     = findViewById<GridLayout>(R.id.GridLayout)
 
-        fnishButton.visibility    = View.GONE
+
         endTimeText.visibility    = View.GONE
         gridLayout.visibility     = View.GONE
         kartNextButton.visibility = View.GONE
-        totalWrongText.visibility = View.GONE
+        totalCorText.visibility   = View.GONE
+        sayiNextButton.visibility = View.GONE
+        corSizeText.visibility    = View.GONE
     }
 }
 
